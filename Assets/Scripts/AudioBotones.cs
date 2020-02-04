6f58c466815666814038778f64f45796d0162dc6
ecollazodominguez <ecollazodominguez@danielcastelao.org>
2020-02-04 20:35:51 +0100

    ArteInteractiva v0.1

---
+++ Assets/Scripts/AudioBotones.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class AudioBotones : MonoBehaviour{
+
+public int isPlaying=0;
+
+    public void Start()
+ {  
+     
+         }
+
+    public void action(){
+        AudioSource audio = GetComponent<AudioSource>();
+        Button b = GetComponent<Button>();
+        if(isPlaying == 0){
+        isPlaying= 1;
+        audio.Play();
+        }else{
+        isPlaying= 0;
+         audio.Stop();
+    }
+
+}
+}
---
+++ Assets/Scripts/CuadroInterfaz2.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CuadroInterfaz2 : MonoBehaviour, ITrackableEventHandler {
+
+    public GameObject canvas;
+    public GameObject boton1;
+    public GameObject boton2;
+    public GameObject boton3;
+    public GameObject boton4;
+    public GameObject boton5;
+    public GameObject boton6;
+
+
+    public AudioSource audio;
+
+   private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+
+        canvas.SetActive(false);
+        boton1.SetActive(false);
+        boton2.SetActive(false);
+        boton3.SetActive(false);
+        boton4.SetActive(false);
+        boton5.SetActive(false);
+        boton6.SetActive(false);
+
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+
+        if (mTrackableBehaviour)
+        {
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+    
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+			// Play audio when target is found
+			audio = GetComponent<AudioSource>();
+            audio.Play();
+            canvas.SetActive(true);
+            boton3.SetActive(true);
+            boton4.SetActive(true);
+        }
+        else
+        {
+			// Stop audio when target is lost
+			audio.Stop();
+            canvas.SetActive(false);
+            boton3.SetActive(false);
+            boton4.SetActive(false);
+        }
+    }
+}
